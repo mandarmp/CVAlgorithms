a41e2cc48191329d7e9bc23202f3e3d0e76206e9
MANDAR PATIL <33731455+mandarmp@users.noreply.github.com>
2022-03-14 23:36:03 +0000

    Add files via upload

--- image_segmentation.m
+++ image_segmentation.m
@@ -4,8 +4,9 @@ close all
 warning off
 
 dbstop if error
-x=imread('image.png');
-imshow(x);
+x_org=imread('Images/image.png');
+subplot(2,3,1)
+imshow(x_org);
 title('Original Image');
 %%
 % [BW,maskedRGBImage] = createMask(x);
@@ -40,16 +41,19 @@ title('Original Image');
 
 %%
 
-xblur1 = imgaussfilt(x,2);
+xblur1 = imgaussfilt(x_org,2);
+subplot(2,3,2)
 imshow(xblur1)
 xGray = rgb2gray(xblur1);
+subplot(2,3,3)
 imshow(xGray)
 xBW = imbinarize(xGray);
 xBW = imfill(xBW,'holes');
+subplot(2,3,4)
 imshow(xBW)
 [B,L,N,A] = bwboundaries(xGray);
-figure(2);
-imshow(x); hold on;
+subplot(2,3,5)
+imshow(x_org); hold on;
 colors=['b' 'g' 'r' 'c' 'm' 'y'];
 %ref: https://uk.mathworks.com/help/images/ref/bwboundaries.html 
 for k=1:length(B),
@@ -64,34 +68,41 @@ for k=1:length(B),
   h = text(col+1, row-1, num2str(L(row,col)));
   set(h,'Color',colors(cidx),'FontSize',14,'FontWeight','bold');
 end
-g = regionprops(xBW, 'Area','BoundingBox');
+g = regionprops(xBW,xGray, 'Area','BoundingBox','WeightedCentroid','PixelValues');
 
-g(1)
-area_values = [g.Area]
+M = containers.Map();
 
-% %%
-% colorNames = { 'red','green'};
-% %colorNames = { 'red','green','purple','blue','yellow' };
-% nColors = length(colorNames);
-% [rows, columns, numberOfColorChannels] = size(x);
-% sample_regions = false([rows columns nColors]);
-% 
-% 
-% % Convert the fabric RGB image into an L*a*b image.
-% cform = makecform('srgb2lab');
-% lab_image = applycform(x,cform);
-% imshow(lab_image)
-% 
-% % Calculate the mean 'a' and 'b' value for each area extracted.
-% % These values serve as your color markers in 'a*b' space.
-% a = lab_image(:,:,2);
-% b = lab_image(:,:,3);
-% color_markers = repmat(0, [nColors, 2]);
-% 
-% for count = 1:nColors
-%   color_markers(count,1) = mean2(a(sample_regions(:,:,count)));
-%   color_markers(count,2) = mean2(b(sample_regions(:,:,count)));
-% end
-% 
-% % For example, the average color of the second sample region in 'a*b' space is:
-% disp( sprintf('[%0.3f,%0.3f]', color_markers(2,1), color_markers(2,2)) );
+subplot(2,3,5);hold on;
+red = 0;
+green =0;
+for iter_variable = 1: N
+    g(iter_variable)
+    area_values = [g.Area];
+    pixel_values{iter_variable} = g.PixelValues;
+    centroid_value = cast([g.WeightedCentroid],"uint32");
+    XCO = centroid_value(2*(iter_variable)-1);
+    YCO = centroid_value(2*(iter_variable));
+    cval = x_org(YCO,XCO,:);
+    plot(XCO,YCO,'b*')
+    RGB{iter_variable} = [diag(cval(:,:,1)), diag(cval(:,:,2)), diag(cval(:,:,3))];
+    RGB{iter_variable} = double(RGB{iter_variable})./ double(255);
+    [~, index] = max(RGB{1,iter_variable});
+    if index == 1
+        red = red +1;
+    else index == 2
+        green = green +1;
+    end
+    M(num2str(iter_variable))=ans;
+
+end
+
+
+
+if(red>green)
+    countstr = "green is the odd colour"
+else
+    countstr = "red is odd colour"
+end
+ax = subplot(2,3, 6);
+text(0.2,0.2,countstr);
+set ( ax, 'visible', 'off')
